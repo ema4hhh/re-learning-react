a3964bb2b080dcb0478b543030757074b293387a
Emanuel <ema4hhh@gmail.com>
2023-02-18 19:10:20 -0500

    class components

--- src/App.jsx
+++ src/App.jsx
@@ -83,6 +83,72 @@ const Input = styled.input`
   padding: 0.2rem;
 `;
 
+/* class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: "",
+      pokemon: [],
+      selectedItem: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:5173/pokemon.json')
+      .then(response => response.json())
+      .then(data => this.setState({
+        ...this.state,
+        pokemon: data,
+      }));
+  }
+
+  render() {
+    return (
+      <Container>
+        <Title>Pokemon Search</Title>
+        <TwoColumnLayout>
+          <div>
+            <Input
+              value={this.state.filter}
+              onChange={(evt) => this.setState({
+                ...this.state,
+                filter: evt.target.value
+              })}
+            />
+            <table width="100%">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Type</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.pokemon
+                  .filter((pokemon) =>
+                    pokemon.name.english.toLowerCase().includes(this.state.filter.toLowerCase())
+                  )
+                  .slice(0, 20)
+                  .map((pokemon) => (
+                    <PokemonRow
+                      pokemon={pokemon}
+                      onSelect={(pokemon) => this.setState({
+                        ...this.state,
+                        selectedItem: pokemon
+                      })} 
+                      key={pokemon.id} />
+                  ))}
+              </tbody>
+            </table>
+          </div>
+          {this.state.selectedItem && (
+            <PokemonInfo {...this.state.selectedItem} />
+          )}
+        </TwoColumnLayout>
+      </Container>
+    );
+  }
+} */
+
 function App() {
   const [filter, setFilter] = useState('');
   const [selectedItem, setSelectedItem] = useState(null);
